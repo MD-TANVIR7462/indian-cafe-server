17ec99327d32162809e216ea77edcd3571a052fd
unknown <mdtanvir7462@gmail.com>
2023-05-02 02:30:04 +0600

    adding vercel config and deploy in vercel

--- index.js
+++ index.js
@@ -5,4 +5,10 @@ const chef = require('./chef.json')
 app.get('/',(req,res)=>{
 res.send(chef)
 })
+app.get('/:id',(req,res)=>{
+   const id = parseInt(req.params.id)
+   const singleChef = chef.find(chefOnly => chefOnly.id === id)||{}
+   res.send(singleChef)
+
+})
 app.listen(port)
