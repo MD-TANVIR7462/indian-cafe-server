a69a8170f18fa4537e1de32424084588cddda98b
unknown <mdtanvir7462@gmail.com>
2023-05-02 02:13:18 +0600

    all chef data added on port

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
 const port = process.env.port||5000;
+const chef = require('./chef.json')
 app.get('/',(req,res)=>{
-res.send('my-server-is-running')
+res.send(chef)
 })
 app.listen(port)
